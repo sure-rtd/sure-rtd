ef800182f9c2b9103710d947077b20a2c18b0db4
SURE <sikatomosiren@gmail.com>
2016-04-01 15:43:50 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
-var startButton = document.getElementById('start');
-var displayArea = document.getElementById('display');
 var slotTime = null;
 var slotNumber = 0;
 var nMax = 82;
@@ -10,23 +8,27 @@ var units = ['1','2','3','4','5','6','7','8','9','10',
           '11','12','13','14','15','16','17','18','19','20'];
 
 function start() {
+	var startButton = document.getElementById('start');
+	var displayArea = document.getElementById('display');
+
 	if(statusFlg==='stop'){
         
         //ステータスを開始状態に
-    		statusFlg = 'start';
+	statusFlg = 'start';
 
         clearInterval(slotTime);
 
-  		//ランダムな整数を発生してタイマー秒間隔で無限にループ
-      slotTime = setInterval(function () {
-			slotNumber = Math.floor(Math.random()*(nMax-nMin+1))+nMin;
-      displayArea.value = units[slotNumber];
+	//ランダムな整数を発生してタイマー秒間隔で無限にループ
+	slotTime = setInterval(function () {
+	slotNumber = Math.floor(Math.random()*(nMax-nMin+1))+nMin;
+      	displayArea.value = units[slotNumber];
       }, 10);
     
       //ボタンの表示をSTOPに変更
-      startButton.innerHTML = 'STOP';
+      startButton.value = 'STOP';
+      
 }else if(statusFlg==='start'){
-	  	clearInterval(slotTime);
+  	clearInterval(slotTime);
     	slotTime = null;
 
     //ステータスを停止状態に
@@ -36,7 +38,7 @@ function start() {
 		units.splice(slotNumber,1);
 		nMax -= 1;
     //ボタンの表示をSTARTに変更
-    startButton.innerHTML = 'START';
+    startButton.value = 'START';
 	}
 }
 
